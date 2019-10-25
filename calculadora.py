21b01e86900528eb0d9e9cdfa22cf93f80d056c8
DaLeste777 <44482365+DaLeste777@users.noreply.github.com>
2019-10-25 12:41:43 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+num = int(input("Valor:"))
+num2 = int(input("Valor:"))
+while True:
+    if num + num2:
+        print("Soma")
+    elif num - num2
+        print("Subtração")
+    elif num * num2
+        print("Multiplicação")
+    elif num / num2
+        print("Divisão")
